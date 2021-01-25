c3089789193cfa2b4d5014746e4e0f920ecbc0a6
BananeIT <77931084+BananeIT@users.noreply.github.com>
2021-01-25 18:10:57 +0100

    Update help.js

--- commandes/help.js
+++ commandes/help.js
@@ -14,7 +14,7 @@ module.exports = {
             .setThumbnail("https://cdn.discordapp.com/avatars/791364666990264411/db9046775198265328ffd6267b17c37f.png?size=128")
             .addField("Commandes moderations:crossed_swords:", "`?kick @utilisateur` \n\n `?ban @utilisateur` \n\n `?mute @utilisateur` \n\n `?unmute @utilisateur` \n\n `?clear nombre de message à supprimer` \n\n `?poll sujet du sondage` ", true)
             .addField("\u200B", "\u200B", true)
-            .addField("Commande fun:partying_face:", "`?ping` \n\n `?mp @utilisateur "message"`", true)
+            .addField("Commande fun:partying_face:", "`?ping` \n\n `?mp @utilisateur <message>`", true)
             .addField("\u200B", "\u200B", true)
             .addField("Commande autre:person_standing:", "`?stats @utilisateur`", true)
             .addField("Commande secret:shushing_face:", "`Chuut...`", false)
