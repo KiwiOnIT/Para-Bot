a578423291caddb717ff5a557081b63b3c1e18d1
BananeIT <77931084+BananeIT@users.noreply.github.com>
2021-01-26 14:35:05 +0100

    Add files via upload

---
+++ commandes/bo.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client;
+const { color1 } = require('./../config.json');
+module.exports = {
+    name: "bo",
+    category: "commandes",
+    description: "Retourne le handler",
+    run: async (Client, message, args) => {
+        message.delete();
+        if (message.author.bot) return;
+        if (message.channel.type == "dm") return;
+        var embed15 = new Discord.MessageEmbed()
+            .setColor(color1)
+            .setTitle("Commande secret")
+            .addField("toi aussi", "t bo", false)
+            .setFooter(message.author.username)
+            .setTimestamp()
+        message.reply(embed15)
+    }
+}
