97065a2670ebe5ad6aff175ffc30bf5029720a3d
BananeIT <77931084+BananeIT@users.noreply.github.com>
2021-01-26 11:55:48 +0100

    Update avatar.js

--- commandes/avatar.js
+++ commandes/avatar.js
@@ -6,6 +6,7 @@ module.exports = {
     category: "commandes",
     description: "Retourne le handler",
     run: async (Client, message, args) => {
+        message.delete();
     const embed = new Discord.MessageEmbed()
 
     if(!message.mentions.users.first()){
