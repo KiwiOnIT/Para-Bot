eb316435a5bf769987b50376ab870a1e63c821f6
BananeIT <77931084+BananeIT@users.noreply.github.com>
2021-01-26 11:54:28 +0100

    Add files via upload

---
+++ commandes/avatar.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client;
+const { color1 } = require('./../config.json');
+module.exports = {
+    name: "avatar",
+    category: "commandes",
+    description: "Retourne le handler",
+    run: async (Client, message, args) => {
+    const embed = new Discord.MessageEmbed()
+
+    if(!message.mentions.users.first()){
+        embed.setTitle("Ton Avatar:")
+        embed.setImage(message.author.displayAvatarURL())
+        embed.setColor(color1)
+        .setFooter(message.author.username)
+        .setTimestamp()
+        return message.channel.send(embed)
+    }else{
+        const user = message.mentions.users.first()
+        embed.setTitle(`Avatar de ${user.tag}:`)
+        embed.setImage(user.displayAvatarURL())
+        embed.setColor(color1)
+        .setFooter(message.author.username)
+        .setTimestamp()
+        return message.channel.send(embed)
+    }
+}
+}
