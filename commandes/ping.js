6cf1876804e89fdea9aa844e3985c72813441767
BananeIT <77931084+BananeIT@users.noreply.github.com>
2021-02-10 12:14:55 +0100

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -7,9 +7,19 @@ module.exports = {
     description: "Retourne le handler",
     run: async (Client, message, args) => {
         message.delete();
-        if (message.author.bot) return;
-        if (message.channel.type == "dm") return;
         let début = Date.now();
-        await message.channel.send(":ping_pong:  Ping").then(async (m) => await m.edit(`:ping_pong:  Pong : **${Date.now() - début} ms**`));
-    }
-}
+
+        let before = new Discord.MessageEmbed()
+        .setColor(color1)
+        .setDescription(":ping_pong: | Ping")
+    
+        await message.channel.send(before).then(async(m) => {
+            let ping = Date.now() - début
+    
+            let after = new Discord.MessageEmbed()
+            .setColor(color1)
+            .setDescription(`:ping_pong: | Pong : **${ping} ms**`)
+    
+            await m.edit(after);
+        }
+)}};
