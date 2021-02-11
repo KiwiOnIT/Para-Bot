92b515e6b4aa5288c5f4d700e39e82d7fbce0ff9
BananeIT <77931084+BananeIT@users.noreply.github.com>
2021-02-11 18:35:19 +0100

    Update poll.js

--- commandes/poll.js
+++ commandes/poll.js
@@ -6,24 +6,28 @@ module.exports = {
     category: "commande",
     description: "Returns latency and API ping",
     run: async (Client, message, args) => {
-        message.delete();
         if (message.member.hasPermission("ADMINISTRATOR")) {
             let args = message.content.split(" ").slice(1);
             let thingToEcho = args.join(" ")
             const embed = new Discord.MessageEmbed()
                 .setTitle("📊 __**Sondage**__")
-                .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
+                .addField(thingToEcho, "Répondre avec <a:check:803019851614388225> ou <a:red_cross:802974815073206322>")
                 .setColor(color1)
                 .setFooter(message.author.username)
                 .setTimestamp()
             message.channel.send(embed)
                 .then(function (message) {
-                    message.react("✅")
-                    message.react("❌")
+                    message.react("803019851614388225")
+                    message.react("802974815073206322")
                 }).catch(function () {
                 });
+             
         } else {
-            return message.reply("Tu n'as pas la permission")
-        }
+            var embed15 = new Discord.MessageEmbed()
+            .setTitle("<a:red_cross:802974815073206322> tu n'as pas la permission")
+            .setColor(color1)
+             return message.channel.send(embed15)
+            
+        } 
     }
 }
