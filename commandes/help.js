728589e937371c00e0610ea17a8b0b7599f127a1
BananeIT <77931084+BananeIT@users.noreply.github.com>
2021-07-18 10:58:35 +0200

    Update help.js

--- commandes/help.js
+++ commandes/help.js
@@ -13,7 +13,7 @@ module.exports = {
             .setThumbnail("https://cdn.discordapp.com/attachments/688772025321521204/848673499890974730/image0.png")
             .addField("Commandes moderations:crossed_swords:", "`?kick @user` \n\n `?ban @user` \n\n `?mute @user` \n\n `?unmute @user` \n\n `?clear nombre de message à supprimer` \n\n `?poll sujet du sondage \n\n ?tempmute temps en ms \n\n ?clone`", true)
             .addField("\u200B", "\u200B", true)
-            .addField("Commande public :person_standing:", "`?stats @user` \n\n `?infobot` \n\n `?ping` \n\n `?dm @user` \n\n `?juif @user` \n\n `?gay @user` \n\n `?wiki recherche à faire` \n\n `?pourcentage `", true)
+            .addField("Commande public :person_standing:", "`?stats @user` \n\n `?infobot` \n\n `?ping` \n\n `?dm @user` \n\n `?wiki recherche à faire` \n\n ``?pourcentage `", true)
             .addField("Commande secret:shushing_face:", "`Chuut...`", true)
             .setFooter(message.author.username)
             .setTimestamp()
